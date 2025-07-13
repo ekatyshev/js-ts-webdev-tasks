412dbd233d06efdef69b80682e1cda48cb1a9c06
u_shumi <u.shumilkina@yandex.ru>
2025-07-13 16:47:50 +0400

    Feat: CSS, layout

--- src/components/footer/Footer.ts
+++ src/components/footer/Footer.ts
@@ -1,67 +1,72 @@
 import './Footer.sass';
 
 export function Footer(): HTMLElement {
-    const footer = document.createElement('footer')
+  const footer = document.createElement('footer')
+  footer.classList.add('footer__wrapper')
 
     footer.innerHTML = `
-<div class="subscription">
-    <form>
+<div class="footer content-max-width">
+    <form class="subscription">
         <h4>STAY UPTO DATE ABOUT OUR LATEST OFFERS</h4>
-        <input class="email" type="email" placeholder="Enter your email address">
-        <button>Subscribe to Newsletter</button>    
+        <div class="right-block">
+          <div class="email__wrapper">
+            <input class="email" type="email" placeholder="Enter your email address">
+          </div>
+          <button>Subscribe to Newsletter</button>
+        </div>    
     </form>
-</div>
-
-<nav class="footer-navigation">
-    <div class="footer__about">
-        <h5>Shop.co</h5>
-        <div class="slogan">We have clothes that suits your style and which you’re proud to wear. From women to men.</div>
-        <ul class="social-links">
-            <li class="social-link social-link_twitter"></li>
-            <li class="social-link social-link_facebook"></li>
-            <li class="social-link social-link_instagram"></li>
-            <li class="social-link social-link_github"></li>
-        </ul>
-    </div>
-    <ul>
-        <h5>Company</h5>
-        <li>About</li>
-        <li>Features</li>
-        <li>Works</li>
-        <li>Career</li>
-    </ul>
-    <ul>
-        <h5>Help</h5>
-        <li>Customer Support</li>
-        <li>Delivery Details</li>
-        <li>Terms & Conditions</li>
-        <li>Privacy Policy</li>
-    </ul>
-    <ul>
-        <h5>FAQ</h5>
-        <li>Account</li>
-        <li>Manage Deliveries</li>
-        <li>Orders</li>
-        <li>Payments</li>
-    </ul>
-    <ul>
-        <h5>Resources</h5>
-        <li>Free eBooks</li>
-        <li>Development Tutorial</li>
-        <li>How to - Blog</li>
-        <li>Youtube Playlist</li>
-    </ul>
-</nav>
-
-<div class="bottom-line">
-    <div class="copyright">Shop.co © 2000-2023, All Rights Reserved</div>
-    <ul class="payment-options">
-        <li class="payment-option payment-option_visa"></li>
-        <li class="payment-option payment-option_mastercard"></li>
-        <li class="payment-option payment-option_paypal"></li>
-        <li class="payment-option payment-option_apple"></li>
-        <li class="payment-option payment-option_google"></li>
-    </ul>
+  
+  <nav class="footer-navigation">
+      <div class="footer__about">
+          <h4>Shop.co</h4>
+          <div class="slogan">We have clothes that suits your style and which you’re proud to wear. From women to men.</div>
+          <ul class="social-links">
+              <li class="social-link social-link_twitter"></li>
+              <li class="social-link social-link_facebook"></li>
+              <li class="social-link social-link_instagram"></li>
+              <li class="social-link social-link_github"></li>
+          </ul>
+      </div>
+      <ul>
+          <h5>Company</h5>
+          <li class="footer-navigation__list-item">About</li>
+          <li class="footer-navigation__list-item">Features</li>
+          <li class="footer-navigation__list-item">Works</li>
+          <li class="footer-navigation__list-item">Career</li>
+      </ul>
+      <ul>
+          <h5>Help</h5>
+          <li class="footer-navigation__list-item">Customer Support</li>
+          <li class="footer-navigation__list-item">Delivery Details</li>
+          <li class="footer-navigation__list-item">Terms & Conditions</li>
+          <li class="footer-navigation__list-item">Privacy Policy</li>
+      </ul>
+      <ul>
+          <h5>FAQ</h5>
+          <li class="footer-navigation__list-item">Account</li>
+          <li class="footer-navigation__list-item">Manage Deliveries</li>
+          <li class="footer-navigation__list-item">Orders</li>
+          <li class="footer-navigation__list-item">Payments</li>
+      </ul>
+      <ul>
+          <h5>Resources</h5>
+          <li class="footer-navigation__list-item">Free eBooks</li>
+          <li class="footer-navigation__list-item">Development Tutorial</li>
+          <li class="footer-navigation__list-item">How to - Blog</li>
+          <li class="footer-navigation__list-item">Youtube Playlist</li>
+      </ul>
+  </nav>
+  
+  <div class="bottom-line">
+      <div class="copyright">Shop.co © 2000-2023, All Rights Reserved</div>
+      <ul class="payment-options">
+          <li class="payment-option payment-option_visa"></li>
+          <li class="payment-option payment-option_mastercard"></li>
+          <li class="payment-option payment-option_paypal"></li>
+          <li class="payment-option payment-option_apple"></li>
+          <li class="payment-option payment-option_google"></li>
+      </ul>
+  </div>
 </div>
 
 `
