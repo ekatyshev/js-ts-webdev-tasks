ad62fab233bbe2c664ab115e891dd7fd21a73a9a
u_shumi <u.shumilkina@yandex.ru>
2025-07-13 14:49:19 +0400

    Feat: CSS, layout

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -18,38 +18,6 @@ export function Header(): HTMLElement {
         </div>
     </div>
 </div>
-
-<div class="cover__wrapper">
-    <div class="cover content-max-width">
-        <div class="cover__slogan">FIND <u>ANYTHING</u> THAT MATCHES YOUR STYLE</div>
-        <div class="cover__description">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</div>
-        <a class="show-now" href="#">Shop Now</a>
-        <ul class="numbers">
-            <li>
-                <div>200+</div>
-                <p>International Brands</p>
-            </li>
-            <li>
-                <div>2,000+</div>
-                <p>High-Quality Products</p>
-            </li>
-            <li>
-                <div>30,000+</div>
-                <p>Happy Customers</p>
-            </li>
-        </ul>
-    </div>
-</div>
-
-<div class="brand-band__wrapper">
-  <ul class="brand-band content-max-width">
-      <li class="brand-band__item brand-band__item_versace"></li>
-      <li class="brand-band__item brand-band__item_zara"></li>
-      <li class="brand-band__item brand-band__item_gucci"></li>
-      <li class="brand-band__item brand-band__item_prada"></li>
-      <li class="brand-band__item brand-band__item_calvin-klein"></li>
-  </ul>
-</div>
 `
 
     const logo = header.querySelector('#logo');
